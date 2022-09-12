efe4c494a9e4ba701873d1975a4ad8658c02e4d3
Daniel Brice <danielbrice@gmail.com>
2022-09-12 15:27:06 -0700

    add installation instruction

--- README.md
+++ README.md
@@ -6,14 +6,27 @@ Read diagnostics from hlint.json
 
 The extension watches `hlint.json` for changes, parses its content, and reports its findings to VS Code as diagnostics.
 
-This plugin does not run Hlint for you (though future versions might).
+This extension does not run Hlint for you (though future versions might).
 You can run Hlint on file save using the `--test` option for Ghcid.
 
 ```console
 ghcid --test ":! hlint . --json > hlint.json"
 ```
 
+## Install
+
+This extension isn't published on the market place (though future versions might be).
+To install this extension, clone the repo and follow these setps.
+
+```console
+npm install
+npm run compile
+npm run package
+npx vsce package
+code --install-extension vscode-watch-hlint-0.0.1.vsix
+```
+
 ## Credits
 
 Most of this code was basically vendored from [VSCode haskell-ghcid](https://github.com/ndmitchell/ghcid/tree/master/plugins/vscode).
-Thanks to [ndmitchell](https://github.com/ndmitchell), [chrismwendt](https://github.com/chrismwendt), and [yairchu](https:/github.com/yairchu).
+Thanks to [ndmitchell](https://github.com/ndmitchell), [chrismwendt](https://github.com/chrismwendt), and [yairchu](https://github.com/yairchu).
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
+import { Uri, Diagnostic, Range, DiagnosticSeverity, ExtensionContext, DiagnosticCollection, workspace, languages } from 'vscode';
+import { dirname } from 'path';
+import { readFileSync } from 'fs';
 
 interface Hint {
   severity: 'Ignore' | 'Suggestion' | 'Warning' | 'Error';
@@ -17,8 +17,8 @@ function pair<a, b>(a: a, b: b): [a, b] {
   return [a, b];
 }
 
-function groupDiagnostic(xs: Array<[vscode.Uri, Array<vscode.Diagnostic>]>): Array<[vscode.Uri, Array<vscode.Diagnostic>]> {
-  let seen = new Map<string, [number, vscode.Uri, vscode.Diagnostic[]]>();
+function groupDiagnostic(xs: Array<[Uri, Array<Diagnostic>]>): Array<[Uri, Array<Diagnostic>]> {
+  let seen = new Map<string, [number, Uri, Diagnostic[]]>();
   for (var i = 0; i < xs.length; i++) {
     let key = xs[i][0].path;
     let v = seen.get(key);
@@ -28,39 +28,39 @@ function groupDiagnostic(xs: Array<[vscode.Uri, Array<vscode.Diagnostic>]>): Arr
   return Array.from(seen.values()).sort((a, b) => a[0] - b[0]).map(x => pair(x[1], x[2]));
 }
 
-function parseHlintOutput(dir: string, s: Array<Hint>): Array<[vscode.Uri, Array<vscode.Diagnostic>]> {
+function parseHlintOutput(dir: string, s: Array<Hint>): Array<[Uri, Array<Diagnostic>]> {
   return s.map(hint => {
-    const file = vscode.Uri.file(dir + hint.file);
-    const range = new vscode.Range(hint.startLine, hint.startColumn, hint.endLine, hint.endColumn);
+    const file = Uri.file(dir + hint.file);
+    const range = new Range(hint.startLine, hint.startColumn, hint.endLine, hint.endColumn);
     const message = hint.hint;
     const severity = {
-      'Ignore': vscode.DiagnosticSeverity.Information,
-      'Suggestion': vscode.DiagnosticSeverity.Hint,
-      'Warning': vscode.DiagnosticSeverity.Warning,
-      'Error': vscode.DiagnosticSeverity.Error
+      'Ignore': DiagnosticSeverity.Information,
+      'Suggestion': DiagnosticSeverity.Hint,
+      'Warning': DiagnosticSeverity.Warning,
+      'Error': DiagnosticSeverity.Error
     }[hint.severity];
-    const diag = new vscode.Diagnostic(range, message, severity);
+    const diag = new Diagnostic(range, message, severity);
     return pair(file, [diag]);
   })
 }
 
-export async function activate(context: vscode.ExtensionContext) {
-  const watcher = vscode.workspace.createFileSystemWatcher('**/hlint.json');
+export async function activate(context: ExtensionContext) {
+  const watcher = workspace.createFileSystemWatcher('**/hlint.json');
   context.subscriptions.push(watcher);
-  const uri2diags = new Map<string, vscode.DiagnosticCollection>();
+  const uri2diags = new Map<string, DiagnosticCollection>();
   context.subscriptions.push({ dispose: () => Array.from(uri2diags.values()).forEach(diag => diag.dispose()) });
 
-  const onUpdate = (uri: vscode.Uri) => {
-    const diags = uri2diags.get(uri.fsPath) || vscode.languages.createDiagnosticCollection();
+  const onUpdate = (uri: Uri) => {
+    const diags = uri2diags.get(uri.fsPath) || languages.createDiagnosticCollection();
     uri2diags.set(uri.fsPath, diags);
     diags.clear();
-    const dir = path.dirname(uri.fsPath);
-    const content: Array<Hint> = JSON.parse(fs.readFileSync(uri.fsPath, "utf8"));
+    const dir = dirname(uri.fsPath);
+    const content: Array<Hint> = JSON.parse(readFileSync(uri.fsPath, "utf8"));
     const diagnostics = groupDiagnostic(parseHlintOutput(dir, content));
     diags.set(diagnostics);
   }
 
-  (await vscode.workspace.findFiles('**/hlint.json')).forEach(onUpdate);
+  (await workspace.findFiles('**/hlint.json')).forEach(onUpdate);
   watcher.onDidCreate(onUpdate);
   watcher.onDidChange(onUpdate);
   watcher.onDidDelete(uri => {
